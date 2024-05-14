571917425881b92504e9dee58d77fc6e1edeab00
inklemon1412 <jonasnie@getacademy.no>
2024-05-14 12:09:22 +0200

    Add project files.

---
+++ loops oppgave 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace loops_oppgave_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+            int i = 0;
+            while (condition)
+            {
+                if(i < 10)
+                {
+                    Console.WriteLine($"runde nr {i}");
+                    i++;
+                }
+                else
+                {
+                    condition = false;
+                }
+            }
+        }
+    }
+}
